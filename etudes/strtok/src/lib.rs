cf39a90f9633f366a0fff1091b617aab2cafda13
MeyarAeini <mahiar.aeini@gmail.com>
2025-07-09 21:58:31 -0400

    subtyping and variance

--- etudes/strtok/src/lib.rs
+++ etudes/strtok/src/lib.rs
@@ -1,4 +1,4 @@
-pub fn strtok<'a>(input: &'a mut &'a str, delimiter: char) -> &str {
+pub fn strtok<'b>(input: &mut &'b str, delimiter: char) -> &'b str {
     if let Some(i) = input.find(delimiter) {
         let prefix = &input[..i];
         let suffix = &input[(i + delimiter.len_utf8())..];
@@ -25,3 +25,15 @@ mod tests {
         assert_eq!(input, "world");
     }
 }
+
+//Invariants 
+//
+//fn foo(x: &mut &'a str, y: &'a str) {
+//      *x = &y;
+//}
+//
+//let mut x:&'static str = "hello world";
+//let y = String::new("some string");
+//foo(&mut x, &y);
+//drop(y);
+//println!("{}", x);
